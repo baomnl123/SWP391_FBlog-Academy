f5b7d536151ed8b84e2fc25fb96a2fbc4697b85f
baomnl123 <mglongbao@gmail.com>
2023-10-03 06:21:21 +0700

    Make UserRepository to inherit RepositoryBase

--- backend/backend/Repositories/IRepositories/IUserRepository.cs
+++ backend/backend/Repositories/IRepositories/IUserRepository.cs
@@ -5,8 +5,6 @@ namespace backend.Repositories.IRepositories
 {
     public interface IUserRepository
     {
-        public ICollection<User> GetAllUsers();
-        public ICollection<User> GetUsers(int id);
-        public ICollection<User> GetUsers(string username);
+
     }
 }
--- backend/backend/Repositories/Implementors/UserRepository.cs
+++ backend/backend/Repositories/Implementors/UserRepository.cs
@@ -3,21 +3,8 @@ using backend.Repositories.IRepositories;
 
 namespace backend.Repositories.Implementors
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : RepositoryBase<User>
     {
-        public ICollection<User> GetAllUsers()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ICollection<User> GetUsers(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ICollection<User> GetUsers(string username)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
