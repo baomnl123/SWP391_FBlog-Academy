e9fe0900998a78f3700182785093a55a615c780c
baomnl123 <mglongbao@gmail.com>
2023-10-05 17:00:14 +0700

    Solve conflict

--- backend/backend/Models/Comment.cs
+++ backend/backend/Models/Comment.cs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
-
-#nullable disable
 
 namespace backend.Models
 {
@@ -26,32 +23,3 @@ namespace backend.Models
         public virtual ICollection<VoteComment> VoteComments { get; set; }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-
-#nullable disable
-
-namespace backend.Models
-{
-    public partial class Comment
-    {
-        public Comment()
-        {
-            VoteComments = new HashSet<VoteComment>();
-        }
-
-        public int Id { get; set; }
-        public int PostId { get; set; }
-        public int UserId { get; set; }
-        public string Content { get; set; }
-        public bool Status { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-
-        public virtual Post Post { get; set; }
-        public virtual User User { get; set; }
-        public virtual ICollection<VoteComment> VoteComments { get; set; }
-    }
-}
->>>>>>> main
