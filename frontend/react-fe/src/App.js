751dbf4ea56bc3f3b1d5439436996894f1377336
DuydoFE <manhlon18@gmail.com>
2023-10-13 18:34:24 +0700

    Add frontend

---
+++ frontend/react-fe/src/App.js
@@ -0,0 +1,13 @@
+import React , { Fragment } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Navleft from './components/Navleft'
+function App() {
+  return (
+    <Fragment>
+      <Navleft />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/react-fe/src/components/Navleft.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navleft = () => {
+    return(
+    <body>
+        <div class="Menu_options active">
+            <span class="material-icons">home</span>
+            <h2>Home</h2>
+        </div>  
+       
+    </body>
+    )
+}
+export default Navleft
