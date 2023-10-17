16ad7788f3bcddcc2067cc04b4d69279bf29cd5e
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2023-10-17 15:41:48 +0700

    Add all declaration for IVotePostRepository

---
+++ backend/backend/Repositories/IRepositories/IVotePostRepository.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models;
+
+namespace backend.Repositories.IRepositories
+{
+    public interface IVotePostRepository
+    {
+        public VotePost? GetVotePost(int currentUserId, int postId);
+        public ICollection<User>? GetAllUsersVotedBy(int postId);
+        public VotePost? Add(VotePost newVotePost);
+        public VotePost? Update(VotePost votePost);
+        public VotePost? DisableAllVotePostOf(Post post);
+        public bool Save();
+    }
+}
