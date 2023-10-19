6bc8bb88af37c8e19e8921b7b453ca636bf0ee2a
baomnl123 <mglongbao@gmail.com>
2023-10-19 21:23:52 +0700

    Add PostImage and PostVideo

---
+++ backend/backend/Repositories/IRepositories/IPostImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models;
+
+namespace backend.Repositories.IRepositories
+{
+    public interface IPostImageRepository
+    {
+        public ICollection<PostImage> GetPostImagesByPostId(int postId);
+        public ICollection<PostImage> GetPostImagesByImageId(int imageId);
+        public bool CreatePostImage(PostImage postImage);
+        public bool UpdatePostImage(PostImage postImage);
+        public bool EnablePostImage(PostImage postImage);
+        public bool DisablePostImage(PostImage postImage);
+        public bool Save();
+    }
+}
