780a0c7bcc3752f4c38591e76773a6a1cf1ee413
baomnl123 <mglongbao@gmail.com>
2023-10-20 15:59:59 +0700

    Get specific CategoryTag by tagId and categoryId

--- backend/backend/Repositories/IRepositories/ICategoryTagRepository.cs
+++ backend/backend/Repositories/IRepositories/ICategoryTagRepository.cs
@@ -4,6 +4,7 @@ namespace backend.Repositories.IRepositories
 {
     public interface ICategoryTagRepository
     {
+        public CategoryTag GetCategoryTag(int tagId, int categoryId);
         public ICollection<CategoryTag> GetCategoryTagsByTagId(int tagId);
         public ICollection<CategoryTag> GetCategoryTagsByCategoryId(int categoryId);
         public bool CreateCategoryTag(CategoryTag categoryTag);
