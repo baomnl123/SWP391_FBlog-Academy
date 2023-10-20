150ce69a04e016bdd526eb833501489d5d1bfeaf
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2023-10-20 12:42:58 +0700

    set password to be nullable

--- backend/backend/Models/User.cs
+++ backend/backend/Models/User.cs
@@ -26,7 +26,7 @@ namespace backend.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public string Role { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
