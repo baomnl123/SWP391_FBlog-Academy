f192051dea9cc5c5026196f58f6a528371c6a33e
baomnl123 <mglongbao@gmail.com>
2023-10-24 14:29:32 +0700

    Add PostId in ImageDTO and VideoDTO

--- backend/backend/DTO/VideoDTO.cs
+++ backend/backend/DTO/VideoDTO.cs
@@ -3,6 +3,7 @@
     public class VideoDTO
     {
         public int Id { get; set; }
+        public int PostId { get; set; }
         public string Url { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool Status { get; set; }
