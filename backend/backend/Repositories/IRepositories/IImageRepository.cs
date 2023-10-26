778bd587252d522f0c0fda4a118edf34d8e89854
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2023-10-26 13:18:19 +0700

    set allow null for function GetImagesByPost

--- backend/backend/Repositories/IRepositories/IImageRepository.cs
+++ backend/backend/Repositories/IRepositories/IImageRepository.cs
@@ -6,7 +6,7 @@ namespace backend.Repositories.IRepositories
     {
         public Image? GetImageById(int imageId);
         public Image? GetImageByURL(string imageURL);
-        public ICollection<Image> GetImagesByPost(int postId);
+        public ICollection<Image>? GetImagesByPost(int postId);
         public bool CreateImage(Image image);
         public bool UpdateImage(int postId, Image image);
         public bool EnableImage(Image image);
