5638d772c71f2220e19c97cc56cf317d7a345394
DuydoFE <manhlon18@gmail.com>
2023-11-14 00:41:22 +0700

    update side

--- frontend_user/src/pages/UserProfile/index.tsx
+++ frontend_user/src/pages/UserProfile/index.tsx
@@ -11,6 +11,9 @@ import { useCallback, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import ModalListUsers from './components/ModalListUsers'
+import { PendingPost } from '@/types'
+import SiderDashboard, { FilterType } from '../Dashboard/components/SiderDashboard'
+import CreateUpdatePost from '../Dashboard/components/CreateUpdatePost'
 import { User } from '@/types'
 
 export default function UserProfile() {
@@ -20,6 +23,11 @@ export default function UserProfile() {
   const [titleModal, setTitleModal] = useState('Followers')
   const [users, setUsers] = useState<User[]>([])
   const [modal, contextHolder] = Modal.useModal()
+  const [openPost, setOpenPost] = useState(false)
+  const [categories, setCategories] = useState<string | string[] | number | number[] | null>([])
+  const [tag, setTags] = useState<string | string[] | number | number[]>([])
+  const [filter, setFilter] = useState<FilterType | null>(null)
+  const [postFilter, setPostFilter] = useState<PendingPost[] | null>(null)
   // const navigate = useNavigate()
   const { id } = useParams()
   const currentId = useSelector<RootState>((state) => state.userReducer.user?.id)
@@ -227,54 +235,36 @@ export default function UserProfile() {
 
   return (
     <BaseLayout
+      showSearch
       sider={
-        <div>
-          {/* <div className='mb-6'>
-            <SelectLabel
-              label='Tag'
-              placeHolder='Select Tag'
-              optionData={optionsTag}
-              onChange={(value) => {
-                console.log(value)
-              }}
-            />
-          </div>
-          <div className='mb-6'>
-            <SelectLabel
-              label='Category'
-              placeHolder='Select Category'
-              optionData={optionsCategory}
-              onChange={(value) => {
-                console.log(value)
-              }}
-            />
-          </div>
-          <Space
-            className={`mt-8 w-full cursor-pointer ${filter === 'image' ? 'bg-blue-600' : ''} py-2 px-10 rounded-md`}
-            size={10}
-            onClick={() => {
-              setFilter('image')
-            }}
-          >
-            <IconPicture color={isDarkMode ? '#fff' : '#000'} width={30} height={30} />
-            <Typography.Text>Image</Typography.Text>
-          </Space>
-          <Space
-            className={`mt-8 w-full cursor-pointer ${filter === 'video' ? 'bg-blue-600' : ''} py-2 px-10 rounded-md`}
-            size={10}
-            onClick={() => {
-              setFilter('video')
-            }}
-          >
-            <IconPhotoFilm color={isDarkMode ? '#fff' : '#000'} width={30} height={30} />
-            <Typography.Text>Video</Typography.Text>
-          </Space> */}
-          {/* <Button block type='primary' onClick={() => navigate('/')}>
-            Dashboard
-          </Button> */}
-        </div>
+        <SiderDashboard
+          onGetCategories={(data) => {
+            setCategories(data)
+          }}
+          onGetTags={(data) => {
+            setTags(data)
+          }}
+          createPost={() => {
+            setOpenPost(true)
+            setIdPost(undefined)
+          }}
+          onFilter={(data) => {
+            setFilter(data)
+          }}
+        />
       }
     >
+      <CreateUpdatePost
+        isOpen={openPost}
+        id={idPost}
+        setModal={(value) => {
+          setOpenPost(value)
+          setIdPost(undefined)
+        }}
+        onFinish={() => {
+          getPost({})
+        }}
+      />
       <Spin spinning={loading}>
         <div className='max-w-[1200px] mx-auto'>
           <div className='flex gap-5 mb-10  '>
