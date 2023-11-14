f6236774bbf76543855a3f754af73c3f725b9851
PersieDaCoder <thinhdpham2510@gmail.com>
2023-11-14 21:22:45 +0700

    Update index.tsx

--- frontend_admin/src/components/BaseLayout/index.tsx
+++ frontend_admin/src/components/BaseLayout/index.tsx
@@ -119,7 +119,7 @@ const BaseLayout = ({ children }: PropsWithChildren) => {
           <div className='flex justify-center items-center'>
             <Link to='/dashboard'>
               <img
-                src={`/public/${collapsed ? 'sm-logo.png' : 'logo.png'}`}
+                src={`/${collapsed ? 'sm-logo.png' : 'logo.png'}`}
                 className={`${collapsed && 'py-5'}`}
                 alt=''
                 width={collapsed ? 20 : 100}
