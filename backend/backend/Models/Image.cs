1f732630d228ae6765b2781b960d1045de9932f9
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2023-11-16 17:35:39 +0700

    update model Image, SaveList, Video, VotePost

--- backend/backend/Models/Image.cs
+++ backend/backend/Models/Image.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
 namespace backend.Models
 {
+    [Table("Image")]
     public partial class Image
     {
         public Image()
         {
         }
 
+        [NotNull]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [NotNull]
+        [ForeignKey("Post")]
         public int PostId { get; set; }
+
+        [NotNull]
+        [MaxLength]
         public string Url { get; set; }
+
+        [NotNull]
         public DateTime CreatedAt { get; set; }
+
+        [NotNull]
         public bool Status { get; set; }
 
         public virtual Post Post { get; set; }
--- backend/backend/Models/VotePost.cs
+++ backend/backend/Models/VotePost.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
 namespace backend.Models
 {
+    [Table("VotePost")]
     public partial class VotePost
     {
+        [NotNull]
+        [Key]
+        [ForeignKey("User")]
         public int UserId { get; set; }
+
+        [NotNull]
+        [Key]
         public int PostId { get; set; }
-        [Required]
+
+        [NotNull]
         [Range(0, 2)]
         public int Vote { get; set; }
+
+        [NotNull]
         public DateTime CreatedAt { get; set; }
 
         public virtual Post Post { get; set; }
