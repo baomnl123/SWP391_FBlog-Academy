130e17153243510408331f974eb448d53eb0d855
PersieDaCoder <thinhdpham2510@gmail.com>
2023-11-17 10:39:20 +0700

    user major update (user has majors)

---
+++ backend/backend/DTO/UserMajorDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace backend.DTO
+{
+    public class UserMajorDTO
+    {
+        public UserDTO? User { get; set; }
+        public MajorDTO? Major { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ backend/backend/Repositories/Implementors/UserMajorRepository.cs
@@ -0,0 +1,75 @@
+﻿using backend.Models;
+using backend.Repositories.IRepositories;
+
+namespace backend.Repositories.Implementors
+{
+    public class UserMajorRepository : IUserMajorRepository
+    {
+        private readonly FBlogAcademyContext _fblogAcademyContext;
+        public UserMajorRepository()
+        {
+            _fblogAcademyContext = new();
+        }
+        public bool Add(UserMajor userMajor)
+        {
+            try
+            {
+                _fblogAcademyContext.UserMajors.Add(userMajor);
+                return _fblogAcademyContext.SaveChanges() != 0;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
+
+        public UserMajor? GetBy(int userID, int majorID)
+        {
+            try
+            {
+                return _fblogAcademyContext.UserMajors.Where(e => e.UserId == userID && e.MajorId == majorID).FirstOrDefault();
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        public ICollection<Major>? GetMajorsOf(int userID)
+        {
+            try
+            {
+                return _fblogAcademyContext.UserMajors.Where(e => e.UserId == userID).Select(e => e.Major).ToList();
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        public ICollection<User>? GetUsersOf(int majorID)
+        {
+            try
+            {
+                return _fblogAcademyContext.UserMajors.Where(e => e.MajorId == majorID).Select(e => e.User).ToList();
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        public bool Update(UserMajor userMajor)
+        {
+            try
+            {
+                _fblogAcademyContext.UserMajors.Update(userMajor);
+                return _fblogAcademyContext.SaveChanges() != 0;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
+    }
+}
