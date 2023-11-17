cf781332f5c5b2be66d2f55381bf5ecf6b756d7b
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2023-11-17 23:33:12 +0700

    edit field (upvote, downvote -> vote) in vote object

--- backend/backend/DTO/VoteCommentDTO.cs
+++ backend/backend/DTO/VoteCommentDTO.cs
@@ -4,8 +4,7 @@
     {
         public UserDTO? User { get; set; }
         public CommentDTO? Comment { get; set; }
-        public bool UpVote { get; set; }
-        public bool DownVote { get; set; }
+        public int Vote { get; set; }
         public DateTime CreateAt { get; set; }
     }
 }
--- backend/backend/DTO/VotePostDTO.cs
+++ backend/backend/DTO/VotePostDTO.cs
@@ -4,8 +4,7 @@
     {
         public UserDTO? User { get; set; }
         public PostDTO? Post { get; set; }
-        public bool UpVote { get; set; }
-        public bool DownVote { get; set; }
+        public int Vote { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
