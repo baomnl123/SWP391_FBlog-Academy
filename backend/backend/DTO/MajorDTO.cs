2c8454f19a79bbe82f84ee5d96e2db9371492c67
PersieDaCoder <thinhdpham2510@gmail.com>
2023-11-21 20:57:05 +0700

    List of SubjectDTO addition to MajorDTO

--- backend/backend/DTO/MajorDTO.cs
+++ backend/backend/DTO/MajorDTO.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int AdminId { get; set; }
         public string? MajorName { get; set; }
+        public List<SubjectDTO>? Subjects { get; set; } = new List<SubjectDTO>();
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public bool Status { get; set; }
